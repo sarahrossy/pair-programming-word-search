06a602db1b5751d707fb6487a57ce68ecb22b397
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-13 23:09:40 +0000

    final working code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let newArray = [];
+    let numPlaceholders = 0; 
+    
+    for (let i = 0; i < matrix.length; i++) {
+       numPlaceholders = matrix[i].length;
     }
-}
+    for (let j = 0; j < numPlaceholders; j++){
+        newArray.push([]);
+    }
+    for (let j = 0; j < newArray.length; j++) { 
+      for (let x = 0; x < matrix.length; x++) { 
+        newArray[j].push(matrix[x][j]); 
+    }
+  }
+  return newArray;
+};
+
+const wordSearch = (letters, word) => { 
+    let transposedLetters = transpose(letters);
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
+    
+    for (row of horizontalJoin) { 
+        if (row.includes(word)) {
+            return true;
+        }     
+    }
+    for (element of verticalJoin) {
+        if (element.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
 
 module.exports = wordSearch
